a8ae6993144c3c9dc3987984557e8d2208f59f54
Amir <herus2006cripto@gmail.com>
2024-07-12 16:56:44 +0330

    test for error in deployment mode

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,6 @@
-import NextAuth from "next-auth"
-import CredentialsProvider from "next-auth/providers/credentials"
+
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 import ConnectDataBase from "@/utils/connectDataBase";
 import ExchangeUser from "@/utils/model";
@@ -7,30 +8,30 @@ import ExchangeUser from "@/utils/model";
 import { compare } from "bcryptjs";
 
 export const authOptions = {
-  session : {strategy : 'jwt'},
-  secret : process.env.NEXTAUTH_SECRET,
+  session: { strategy: 'jwt' },
+  secret: process.env.NEXTAUTH_SECRET,
   providers: [
     CredentialsProvider({
-        async authorize(state){
-            let {email , password} = state;
-            
-            try {
-                await ConnectDataBase();
-            } catch (err) {
-                console.log(err)
-                throw new Error('problem at connecting to Data-base')
-            }
+      async authorize(credentials) {
+        const { email, password } = credentials;
 
-            let user = await ExchangeUser.findOne({email : email})
-
-            if(!user) throw new Error('user does not exist')
-            if(!await compare(password , user.password)) throw new Error('password is incorrect')
-
-            return { email : email , name : user.name}
+        try {
+          await ConnectDataBase();
+        } catch (err) {
+          console.log(err);
+          throw new Error('Problem connecting to the database');
         }
+
+        const user = await ExchangeUser.findOne({ email });
+
+        if (!user) throw new Error('User does not exist');
+        if (!await compare(password, user.password)) throw new Error('Password is incorrect');
+
+        return { email: email, name: user.name };
+      }
     })
   ],
-}
+};
 
-const handler = NextAuth(authOptions)
-export { handler as GET, handler as POST }
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
